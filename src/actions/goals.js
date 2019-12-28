306cc541d1570164e089be83cc9e5e965838ee0a
Michael D. Perini <perinimichaeld@yahoo.com>
2019-12-28 10:01:10 -0500

    actions buildout

---
+++ src/actions/goals.js
@@ -0,0 +1,40 @@
+import API from 'goals-todos-api'
+
+export const ADD_GOAL = 'ADD_GOAL'
+export const REMOVE_GOAL = 'REMOVE_GOAL'
+
+const addGoal = (goal) => {
+    return {
+        type: ADD_GOAL,
+        goal
+    }
+}
+
+const removeGoal = (id) => {
+    return {
+        type: REMOVE_GOAL,
+        id
+    }
+}
+
+export const handleAddGoal = (name, cb ) => {
+    return (dispatch) => {
+        return API.saveGoal(name)
+            .then((goal) => {
+                dispatch(addGoal(goal))
+                cb()
+            })
+            .catch(() => alert(`An error has occurred. Try again.`))
+    }
+}
+
+export const handleDeleteGoal = (goal) => {
+    return (dispatch) => {
+        dispatch(removeGoal(goal.id))
+        return API.deleteGoal(goal.id)
+            .catch(() => {
+                dispatch(addGoal(goal))
+                alert(`An error has occurred. Try again.`)
+            })
+    }
+}
